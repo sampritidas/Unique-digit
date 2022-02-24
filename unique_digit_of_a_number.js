c597d4fa2af273bdd0c699d6c47a09b54a9f4ad0
Sampriti Das <99536474+sampritidas@users.noreply.github.com>
2022-02-24 22:37:56 +0530

    Implemented function names and separated functions
    Separated repeatDigitCount into two functions
    Named isUnique function to uniqueOrNotStatement

--- unique_digit_of_a_number.js
+++ unique_digit_of_a_number.js
@@ -1,40 +1,45 @@
-function intergerDivideBy10(number, subtrahend) {
+function intDivBy10(number, subtrahend) {
   return (number - subtrahend) / 10;  
 };
 
-function repeatedDigitCount(number) {
+function repeatedDigitInSingleScan(remainNumber, lastDigit) {
   let repeatedDigit = 0;
-  while (number > 0) {
-    let lastDigit = number % 10;
-    let remainNumber = intergerDivideBy10(number, lastDigit);
-
-    while (remainNumber > 0) {
-      let rem = remainNumber % 10;
-
-      if (lastDigit === rem) {
-        repeatedDigit = repeatedDigit + 1;
-      }
-      remainNumber = intergerDivideBy10(remainNumber, rem);
+  while (remainNumber > 0) {
+    let rem = remainNumber % 10;
+    if (lastDigit === rem) {
+      repeatedDigit = repeatedDigit + 1;
     }
-    number = intergerDivideBy10(number, lastDigit);
+    remainNumber = intDivBy10(remainNumber, rem);
   }
   return repeatedDigit;
 };
 
-function isUnique(number) {
-  const repeatedDigit = repeatedDigitCount(number);
-  const trueStatement = "True : all unique";
-  const falseStatement = "False : " + repeatedDigit + " repeats";
-  return repeatedDigit === 0 ? trueStatement : falseStatement;
+function totalRepeatedDigitCount(number) {
+  let totalRepeatedDigit = 0;
+  while (number > 0) {
+    let lastDigit = number % 10;
+    let remainNumber = intDivBy10(number, lastDigit);
+    let repeatedDigit = repeatedDigitInSingleScan(remainNumber, lastDigit);
+    totalRepeatedDigit = totalRepeatedDigit + repeatedDigit;
+    number = intDivBy10(number, lastDigit);
+  }
+  return totalRepeatedDigit;
 };
 
-function formatIsUniqueResult(number) {
-  return number + " // " + isUnique(number);
+function uniqueOrNotStatement(number) {
+  const totalRepeatedDigit = totalRepeatedDigitCount(number);
+  const trueStatement = "True : all unique";
+  const falseStatement = "False : " + totalRepeatedDigit + " repeats";
+  return totalRepeatedDigit === 0 ? trueStatement : falseStatement;
+};
+
+function formatUniqueOrNotResult(number) {
+  return number + " // " + uniqueOrNotStatement(number);
 };
 
 function main() {
-  const number = 1335;
-  console.log(formatIsUniqueResult(number));
+  const number = 12556677;
+  console.log(formatUniqueOrNotResult(number));
 };
 
 main();
